f7ac1359c29dcd06405fd5feeb14a7d8068e9ac0
Warda Arshad <wardaarshad52@gmail.com>
2025-07-03 15:12:32 +0500

    Add files via upload

---
+++ Task 1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int age = 25;
+        string name = "Warda";
+        
+
+        
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        
+    }
+}
