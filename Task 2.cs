a826f28ef8ae7634fbcfd0d9a1f1a931b4360874
Warda Arshad <wardaarshad52@gmail.com>
2025-07-03 15:15:49 +0500

    Add files via upload

---
+++ Task 2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 25, 12, 45, 7, 89, 3, 56 };
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number < min)
+            {
+                min = number;
+            }
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine("Numbers: " + string.Join(", ", numbers));
+        Console.WriteLine("Minimum Value: " + min);
+        Console.WriteLine("Maximum Value: " + max);
+    }
+}
+
