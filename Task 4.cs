f573f655767ace315ead60ee2ce2d65124bb1fc2
Warda Arshad <wardaarshad52@gmail.com>
2025-07-03 15:23:48 +0500

    Add files via upload

---
+++ Task 4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class MyClass
+{
+    
+    public const int MyConst = 10;
+
+    
+    public readonly int MyReadOnly;
+
+    public MyClass(int value)
+    {
+        
+        MyReadOnly = value;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Const Value: " + MyConst);
+        Console.WriteLine("Readonly Value: " + MyReadOnly);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyClass obj = new MyClass(20);
+        obj.Display();
+    }
+}
+
