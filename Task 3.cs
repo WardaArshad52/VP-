1803aa6b9cdfcd9a9b385ae5e1810931103a8bd9
Warda Arshad <wardaarshad52@gmail.com>
2025-07-03 15:23:04 +0500

    Add files via upload

---
+++ Task 3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    // Global variable
+    static int globalNumber = 10;
+
+    static void Main()
+    {
+        // Local variable
+        int localNumber = 5;
+
+        Console.WriteLine("Global number: " + globalNumber);
+        Console.WriteLine("Local number: " + localNumber);
+    }
+}
+
